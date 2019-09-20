1812ed2fe98e6236f202ea4aa4e7062b4eb86530
bob <bobluoluo@hotmail.com>
2019-09-20 19:41:35 -0400

    add shader compiling bath file

---
+++ shaders/compile.bat
@@ -0,0 +1,10 @@
+start glslangValidator.exe -V .\dd_text.vert -o .\dd_text.vert.spv
+start glslangValidator.exe -V .\dd_text.frag -o .\dd_text.frag.spv
+
+start glslangValidator.exe -V .\line_point.vert -o .\line_point.vert.spv
+start glslangValidator.exe -V .\line_point.frag -o .\line_point.frag.spv
+
+start glslangValidator.exe -V .\triangle.vert -o .\triangle.vert.spv
+start glslangValidator.exe -V .\triangle.frag -o .\triangle.frag.spv
+
+pause
